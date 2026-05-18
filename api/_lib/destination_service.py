d5aa080f48aee1789f74fcc9e80df2048cdcb852
gabriel <gabuharada@gmail.com>
2026-05-18 15:30:29 +0900

    merge destination_service.py into recommendation_service.py for now

---
+++ api/_lib/destination_service.py
@@ -0,0 +1,4 @@
+# Destination info is handled by recommendation_service.py
+# TODO: separate destination information from activity recommendations
+#       when time allows, create a dedicated table and service for
+#       general city info (local food, transport, areas to stay)
