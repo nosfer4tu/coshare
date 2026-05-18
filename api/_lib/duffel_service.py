0ad7d7bff592a4d25caaca4f84d768289b733ea6
gabriel <gabuharada@gmail.com>
2026-05-18 14:39:15 +0900

    changed method for importing API, to make code cleaner

--- api/_lib/duffel_service.py
+++ api/_lib/duffel_service.py
@@ -5,17 +5,13 @@ from pathlib import Path
 from dotenv import load_dotenv
 from normalizer import normalize_offers
 from codeshare_service import get_codeshare_offers
-
-env_path = Path(__file__).parent.parent.parent / '.env.local'
-load_dotenv(dotenv_path=env_path)
-
-duffel_access_token = os.getenv("DUFFEL_ACCESS_TOKEN")
+from config import DUFFEL_ACCESS_TOKEN
 
 def search_flights(origin, destination, departure_date):
     response = requests.post(
     "https://api.duffel.com/air/offer_requests?return_offers=true",
     headers={
-        "Authorization": f"Bearer {duffel_access_token}",
+        "Authorization": f"Bearer {DUFFEL_ACCESS_TOKEN}",
         "Content-Type": "application/json",
         "Duffel-Version": "v2",
         "Accept": "application/json"
