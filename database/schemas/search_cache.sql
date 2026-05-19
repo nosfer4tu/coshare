6d59b0ce3a4ee02f13818606b7fdeb89999f2ba2
gabriel <gabuharada@gmail.com>
2026-05-19 13:09:17 +0900

    add passengers and cabin_class to search_cache.sql

--- database/schemas/search_cache.sql
+++ database/schemas/search_cache.sql
@@ -3,6 +3,7 @@ CREATE TABLE search_cache (
     route VARCHAR(20) NOT NULL,
     departure_date DATE NOT NULL,
     passengers INTEGER NOT NULL DEFAULT 1,
+    cabin_class VARCHAR(20) NOT NULL DEFAULT 'economy',
     result_json JSONB NOT NULL,
     cached_at TIMESTAMP DEFAULT NOW()
 );
